ce4d2cb12bb62ddc7c0b902ac9c5b874635c5bf4
CrusaderCree <cpolito@protonmail.com>
2025-04-08 16:19:06 -0500

    calc adding

---
+++ htmllearning/java/java1.js
@@ -0,0 +1,17 @@
+    // This will show text on the webpage
+    document.write("Hello, World!  lousE!");
+    // This will still show in console
+    console.log("Hello, World!");
+    let varname = "Ryan";
+    let varsurname = "Ecklund";
+    let varage = 33
+    varage = 50
+
+    document.write(varname);
+    document.write(varage);
+   
+let animal = {species:"dog"};
+let dog = animal;
+animal = {species:"cat"};
+console.log(animal);
+console.log(dog);
