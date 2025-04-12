bdd754fb44fb5246a057bd2865cd58a75fc1a012
CrusaderCree <cpolito@protonmail.com>
2025-04-12 08:51:06 -0500

    just saving

--- htmllearning/DOM/script.js
+++ htmllearning/DOM/script.js
@@ -16,3 +16,21 @@ para.style.animationDuration = "2s"; // Animation lasts 2 seconds
 para.style.animationIterationCount = "infinite"; // Loops forever
 para.style.animationDirection = "alternate"; // Plays forward, then backward, repeating
 container.appendChild(para);
+
+// create a blue <h3>
+const fonttype  = document.createElement("h3");
+fonttype.textContent = "Hey, I'm a blue <h3>!!";
+fonttype.style.color = "blue";
+container.appendChild(fonttype);
+
+// create black boardered ping background
+const blackBordereddiv  = document.createElement("div");
+const pinkPara = document.createElement("p");
+const newH1= document.createElement("h1");
+newH1.textContent = "I'm in a div";
+pinkPara.textContent = "ME TOO";
+blackBordereddiv.style.backgroundColor = "pink";
+blackBordereddiv.style.border = "black"
+pinkPara.appendChild(blackBordereddiv);
+newH1.appendChild(blackBordereddiv);
+container.appendChild(blackBordereddiv);
