2f8b00d4e22838cc9b84685c4a3c7ddddd920158
CrusaderCree <cpolito@protonmail.com>
2025-05-02 06:51:31 -0500

    dom stuff

--- Treehouse/FunctionsasParameters/app.js
+++ Treehouse/FunctionsasParameters/app.js
@@ -2,13 +2,30 @@ const btnCreate = document.getElementById('btn-main');
 const btnToggle = document.querySelector('.btn-toggle');
 const btnRemove = document.querySelector('.btn-remove');
 const taskList = document.querySelector('.list-container ul');
-const listItems = document.getElementsByTagName('li');
+//const listItems = document.getElementsByTagName('li');
 
-for (let i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener('mouseover', () => {
-        listItems[i].textContent = listItems[i].textContent.toUpperCase();
-    });
-}
+
+taskList.addEventListener('mouseover', (event) => {
+  if(event.target.tagName === 'LI'){
+    event.target.textContent = event.target.textContent.toUpperCase()
+  }
+})
+
+
+
+//on mouseover, make any li item uppercase
+// for (let i = 0; i < listItems.length; i++) {
+//     listItems[i].addEventListener('mouseover', () => {
+//         listItems[i].textContent = listItems[i].textContent.toUpperCase();
+//     });
+// }
+//find out what  target user is clicking on  
+//use just event to see add data (including x and y corrd)
+document.addEventListener('click', (event) => {
+console.log(event.target)
+
+
+})
 
 btnToggle.addEventListener('click', () => {
   const listContainer = document.querySelector('.list-container');
--- Treehouse/FunctionsasParameters/temp.js
+++ Treehouse/FunctionsasParameters/temp.js
@@ -12,3 +12,8 @@ console.log("Bye")
 hiAndBye( () =>{ 
 console.log("Greeting")
 })
+
+// alert to credit the author when copy data from page
+document.addEventListener('copy', (event) => {
+    alert('Please be sure to credit the author')
+});
