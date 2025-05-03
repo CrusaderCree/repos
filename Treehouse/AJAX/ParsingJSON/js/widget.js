a0f9d01ffcfea2711ab6945b7771497db2745030
CrusaderCree <cpolito@protonmail.com>
2025-05-03 10:19:25 -0500

    PARSE JSON

--- Treehouse/AJAX/ParsingJSON/js/widget.js
+++ Treehouse/AJAX/ParsingJSON/js/widget.js
@@ -2,9 +2,46 @@ let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () => { 
   if(xhr.readyState === 4) {
    var employees = JSON.parse(xhr.responseText);
+   let statusHTML = '<ul class="bulleted">';
+   for(let i = 0; i <  employees.length; i++){
+       if( employees[i].inoffice === true) {
+            statusHTML += '<li class="in"<>';
+        } else {
+            statusHTML += '<li class="out"<>';
+        }
+        statusHTML += employees[i].name;
+        statusHTML += '</li>'
+
+   }
+   statusHTML += '</ul>'
+   document.getElementById('employeeList').innerHTML = statusHTML
   }
 
 
 };
 xhr.open('GET','data/employees.json');
 xhr.send();
+
+let xhr_rooms = new XMLHttpRequest();
+xhr_rooms.onreadystatechange = () => { 
+  if(xhr_rooms.readyState === 4) {
+   var rooms = JSON.parse(xhr_rooms.responseText);
+   let statusHTML = '<ul class="bulleted">';
+   for(let i = 0; i <  rooms.length; i++){
+       if( rooms[i].available === true) {
+            statusHTML += '<li class="in"<>';
+        } else {
+            statusHTML += '<li class="out"<>';
+        }
+        statusHTML += rooms[i].room;
+        statusHTML += '</li>'
+
+   }
+   statusHTML += '</ul>'
+   document.getElementById('roomList').innerHTML = statusHTML
+  }
+
+
+};
+xhr_rooms.open('GET','data/rooms.json');
+xhr_rooms.send();
